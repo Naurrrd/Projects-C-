da0a184804983e962544c2113b3fcf88f95512e3
Naurrrd <115657487+Naurrrd@users.noreply.github.com>
2022-10-24 18:52:26 +0300

    Tasks from the second sem

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру (справа налево) 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+string textnum = Convert.ToString(num);
+if (textnum.Length > 2)
+{
+    int third = textnum.Length - 3;
+    Console.Write($"Третья цифра с конца - {textnum[third]}");
+}
+else Console.Write("Третьей цифры нет");
