05ece2a9bdcd9b159da4c13b2fc9bd3ea9b13fd0
Lera <vfubz23@mail.ru>
2022-11-21 17:07:30 +0300

    Sixth seminar

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void CountPositive()
+{
+    Console.Write("Введите несколько чисел через пробел: ");
+    int[] M = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    int positive = 0;
+    for (int i = 0; i < M.Length; i++)
+    {
+        if (M[i] > 0) {positive++;}
+    }
+    Console.WriteLine($"Количество положительных чисел: {positive}");
+}
+
+CountPositive();
---
+++ task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+int[] Line1()
+{
+    Console.Write("Введите k1 и b1: ");
+    int[] y1 = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    return y1;
+}
+int[] Line2()
+{
+    Console.Write("Введите k2 и b2: ");
+    int[] y2 = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    return y2;
+}
+
+void IntersectionPoint(int[] y1, int[] y2)
+{
+    double k1 = y1[0];
+    double b1 = y1[1];
+    double k2 = y2[0];
+    double b2 = y2[1];
+    double x = (b2-b1) / (k1-k2);
+    double y = k1 * x + b1;
+    if (k1 == k2) {Console.Write("Прямые параллельны");}
+    else Console.Write($"Точка пересечения прямых: х = {x}, y = {y}");
+}
+
+IntersectionPoint(Line1(), Line2());
