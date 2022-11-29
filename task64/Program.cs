d4aa512705c8638ed132b188617e78a090c4f38d
Lera <vfubz23@mail.ru>
2022-11-29 23:45:02 +0300

    The last C# seminar

---
+++ task64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64.
+// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+string NaturalNumbers(int N)
+{
+    if (N == 1) return N.ToString();
+    return $"{N}, {NaturalNumbers(N - 1)}";
+}
+
+int N;
+N = 10;
+Console.WriteLine($"N = {N} -> {NaturalNumbers(N)}");
